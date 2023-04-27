bf9ec88335f8378deef345a2e4ccd7a7310c000b
luca betti sorbelli <luca betti sorbelli>
2023-04-27 16:50:44 +0200

    colora di rosso casella comune

--- js/script.js
+++ js/script.js
@@ -3,11 +3,56 @@ const griglia = document.getElementById("grid");
 griglia.classList.add("hidden"); // griglia invisibile di default
 
 bottone.addEventListener("click", function () {
-    griglia.innerHTML=""; // rimuove elementi
+
+    griglia.innerHTML = ""; // rimuove elementi
+
     const select = document.getElementById("select");
+
     const numeroCelle = select.value; // numero celle = valore select
-    creaGriglia (numeroCelle);
+
+    creaGriglia(numeroCelle);
+
     griglia.classList.toggle("hidden");
+
+    // creare 16 numeri random da 1 a 100 tutti diversi
+    function creaNumeroRandom(min, max) {
+        let arrayRandom = [];
+
+        while (arrayRandom.length < 16) {
+
+            let risultatoRandom = Math.floor(Math.random() * (max - min + 1) + min);
+
+            if (!arrayRandom.includes(risultatoRandom)) {
+            arrayRandom.push(risultatoRandom);
+            }
+
+        }
+
+        return arrayRandom;
+    }
+    let numeroBombe = creaNumeroRandom(1, 100);
+    console.log(numeroBombe);
+
+    // associare bombe(n) a numero in tabella
+    
+    // scorrere array casuale
+
+    for(let i = 0; i < numeroBombe.length; i++) {
+        let numeroCasuale = numeroBombe[i];
+
+    // cella corrispondente
+
+    // elemento span con classe "numero" uguale a numero casuale
+    let cella = document.querySelector(`span[numero="${numeroCasuale}"]`);
+
+    //se la cella contiene bomba colora di rosso
+
+    if (cella) {
+        cella.classList.add("alarm");
+        console.log(cella);
+    }
+    }
+
 });
 
 
@@ -21,10 +66,13 @@ function creaGriglia(numeroCelle) {
         let radiceCelle = Math.sqrt(numeroCelle);
         nuovoDiv.style.width = `calc(100% / ${radiceCelle})`;
         nuovoDiv.style.height = `calc(100% / ${radiceCelle})`;
-        
+
         // elemento numerico in casella
         const nuovoSpan = document.createElement("span");
         nuovoSpan.innerText = i;
+
+        // imposta attributo a "nuovoSpan"
+        nuovoSpan.setAttribute("numero", i);
         nuovoDiv.appendChild(nuovoSpan);
 
         nuovoDiv.addEventListener("click", function () {
@@ -33,29 +81,19 @@ function creaGriglia(numeroCelle) {
                 console.log(this.innerText);
             }
             // toggle = (add-remove)
-            this.classList.toggle("clicked"); 
+            this.classList.toggle("clicked");
         });
 
         griglia.appendChild(nuovoDiv);
-        
+
     }
 };
 
-// creare 16 numeri random da 1 a 100 senza doppioni
-function creaNumeroRandom (min, max) {
-    let arrayRandom = [];
-    for (let i = 0; i < 16; i++) {
-    }
-    let NumeroBombe = math.Florr(Math.random() * (max - min) + min);
-    NumeroBombe.push(arrayRandom)
-    return NumeroBombe;
-}
-creaNumeroRandom(1,100);
 
 
-// associare bombe(n) a numero in tabella
 
-//se la cella contiene bomba colora di rosso
+
+
 
 // termina partita quando il giocatore clicca su una bomba o se finisce le celle vuote, comunicare punteggio
 
